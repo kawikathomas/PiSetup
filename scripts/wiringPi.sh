85e4b8332983427abe35575413de5a2e2ddcfd43
Ben McFadden <ben@ministrycentered.com>
2016-10-03 15:16:12 -0700

    Add setup checklist UI. Stub out additional install scripts

---
+++ scripts/wiringPi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if (( $EUID != 0 )); then
+  echo "Error: Must be run as root."
+  exit
+fi
+
+echo -e "WiringPi install script is in development. Check https://github.com/mcfadden/PiSetup for the latest.\n\n\n"
+
+exit
